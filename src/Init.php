c1991a090dbde8f758604185657193df9f263563
madeny <c@madeny.dev>
2019-11-16 23:53:02 +0000

     9 quick fix|updates...

--- src/Init.php
+++ src/Init.php
@@ -16,12 +16,7 @@ class Init {
 		foreach ($this->dirs as $value) {
 
 			if (file_exists($path."/".$value)) {
-				// echo "Path to your certificates >> ".$path."/live"."\n";
-				exec("ls ".$path."/live", $outpout, $error);
-				foreach ($outpout as $value) {
-					// echo $value."\n";
-				}
-				// echo "----------------------- \n";
+
 				return;
 		}else {
 				while ($i < 6) {
@@ -31,7 +26,7 @@ class Init {
 				}
 			}
 		}
-	} // end __construct()
+	}
 
 
 	public function execute($domain)
@@ -42,4 +37,4 @@ class Init {
 		echo shell_exec(__DIR__."/bash/script.sh $path $domain");
 	}
 
-} // end init {}
+}
