a4b562cf3118577486b2f878e97fb01447bd60ac
madeny <c@madeny.dev>
2019-11-17 00:23:31 +0000

     10 quick fix|updates...

---
+++ tests/ConfigTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use Madeny\lhttps\Path;
+use Madeny\lhttps\Config;
+class ConfigTest extends TestCase {
+
+	/** @test */
+	public function it_can_generate_v3 () {
+
+		$path = Path::all();
+
+		$v3 = new Config($path, "madeny.test");
+
+			$this->assertEquals(1, file_exists($path."/cnf/v3.ext"));
+
+	}
+
+
+	   /** @test */
+	   function  it_can_generate_openssl_config()
+	   {
+
+	   	$path = Path::all();
+
+	   	$init = new Config($path, "madeny.dev");
+
+	   		$this->assertEquals(1, file_exists($path."/cnf/openssl.cnf"));
+
+		}
+
+} // InitTest {}
